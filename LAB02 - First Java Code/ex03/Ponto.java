a6d2746cada4abf272d3f3039c5c38e4926bbaea
OtavioMalta <otaviomalta159@hotmail.com>
2022-01-12 22:49:45 -0300

    ex03 Completo

--- LAB02 - First Java Code/ex03/Ponto.java
+++ LAB02 - First Java Code/ex03/Ponto.java
@@ -81,4 +81,11 @@ public class Ponto {
             this.y++;
         }
     }
+
+
+    @Override
+    public String toString() {
+        return "(" + getX() +","+getY()+")"; 
+    }
+
 }
--- LAB02 - First Java Code/ex03/Principal.java
+++ LAB02 - First Java Code/ex03/Principal.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class Principal {
@@ -62,5 +64,48 @@ public class Principal {
         p1.inc_diag("sudoeste");
         System.out.println("sudoeste => " + p1.imprimir_ponto());
     
+        List<Ponto> lista = new ArrayList<Ponto>();
+        
+        System.out.println("Quantos pontos deseja adicionar? ");
+        c = sc.nextInt();
+        int i = 1;
+        while(i<=c){
+            System.out.println("Digite o " + i +" ponto: ");
+            res = sc.next();
+            val = res.split(",");
+            Ponto p = new Ponto();
+            p.setX(Integer.parseInt(val[0]));
+            p.setY(Integer.parseInt(val[1]));
+            lista.add(p);
+            i++;
+        }
+        System.out.println("Pontos: ");
+        lista.forEach(System.out::println);
+        Ponto norte = new Ponto(0, 0);
+        Ponto sul = new Ponto(lista.get(0).getY(), lista.get(0).getY());
+        Ponto leste = new Ponto(0, 0);
+        Ponto oeste = new Ponto(lista.get(0).getX(), lista.get(0).getY());
+
+        for(Ponto p:lista){
+            if(p.getX()>leste.getX()){
+                leste.setX(p.getX());
+                leste.setY(p.getY());
+            }if(p.getY()>norte.getY()){
+                norte.setX(p.getX());
+                norte.setY(p.getY());
+            }if(p.getY()<sul.getY()){
+                sul.setX(p.getX());
+                sul.setY(p.getY());
+            }if(p.getX()<oeste.getX()){
+                oeste.setX(p.getX());
+                oeste.setY(p.getY());
+            }
+        }
+
+        System.out.println("O ponto mais ao norte é " +norte.imprimir_ponto());
+        System.out.println("O ponto mais ao sul é " +sul.imprimir_ponto());
+        System.out.println("O ponto mais ao leste é " +leste.imprimir_ponto());
+        System.out.println("O ponto mais ao oeste é " +oeste.imprimir_ponto());
+
     }
 }
